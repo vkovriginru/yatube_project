c9fe33edca6a6e9adb025f726ea8945a3b378665
vkovriginru <sc@vkovrigin.ru>
2023-05-06 07:57:55 +0300

    итог 3

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='posts_index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа номер {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
